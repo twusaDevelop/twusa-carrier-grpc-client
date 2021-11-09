b05bf63806d920a6e713d4120f341f8132724652
abel <abelwang@twusa.cn>
2021-11-09 13:46:13 +0800

    tests

---
+++ examples/ups_track.php
@@ -0,0 +1,27 @@
+<?php
+$host = '120.26.63.239:7777';
+$client = new \Carrier\Ups\ServiceClient($host,[
+    'credentials' => \Grpc\ChannelCredentials::createInsecure(),
+]);
+$req = new \Carrier\Ups\TrackRequest();
+$auth = new \Carrier\Ups\Auth();
+$auth->setAccessLicenseNumber();
+$auth->setAccountNumber();
+$auth->setPassword();
+$auth->setUsername();
+$req->setAuthorization($auth);
+$req->setTrackNumber('1Z79381232340877123');
+
+/**
+ * @var \Carrier\Ups\TrackResponse $reply
+ * @var \Grpc\Status $status
+ */
+list($reply,$status) = $client->Track($req,metaData())->wait();
+if($status->code != \Grpc\STATUS_OK){
+    echo "{$status->detail}\n";
+    return;
+}
+
+function metaData(){
+    return ['token' => 'KDA88999'];
+}
